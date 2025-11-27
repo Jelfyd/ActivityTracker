eba955d7791ee26fe1573aac688b8399e8cc19af
Babych Andrey <babichandrey79@gmail.com>
2025-11-27 16:44:54 +0200

    TASK-101-2: Added base ActivityTracker class with fields and constructor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class ActivityTracker
+{
+    public int DailyGoal { get; }
+    public int StepsToday { get; private set; }
+
+    public ActivityTracker(int dailyGoal)
+    {
+        DailyGoal = dailyGoal;
+        StepsToday = 0;
+    }
+}
